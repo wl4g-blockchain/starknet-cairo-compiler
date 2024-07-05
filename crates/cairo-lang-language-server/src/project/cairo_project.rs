starkware-libs/cairo

LS: New project model
commit-id:481406b3

crates/cairo-lang-language-server/src/project/cairo_project.rs
@@ -1,0 +1,60 @@
+use std::path::{Path, PathBuf};
+
+use anyhow::Context;
+use cairo_lang_compiler::db::RootDatabase;
+use cairo_lang_compiler::project::update_crate_roots_from_project_config;
+use cairo_lang_project::{ProjectConfig, PROJECT_FILE_NAME};
+use tracing::error;
+
+use super::Project;
+
+/// A [`Project`] manually specified using a `cairo_project.toml` file.
+pub struct CairoProject {
+    /// Path to the `cairo_project.toml` file.p
+    project_path: PathBuf,
+
+    /// Last known revision of `cairo_project.toml` contents, parsed.
+    project_config: Option<ProjectConfig>,
+}
+
+impl CairoProject {
+    /// Constructs a new [`CairoProject`] and loads it.
+    pub fn initialize(project_path: PathBuf) -> Self {
+        assert_eq!(project_path.file_name().and_then(|s| s.to_str()), Some(PROJECT_FILE_NAME));
+        let mut this = Self { project_path, project_config: None };
+        this.reload();
+        this
+    }
+}
+
+impl Project for CairoProject {
+    fn manifest_files(&self) -> Vec<&Path> {
+        vec![&self.project_path]
+    }
+
+    fn reload(&mut self) {
+        let project_config = ProjectConfig::from_file(&self.project_path)
+            .with_context(|| {
+                format!("failed to reload cairo project: {}", self.project_path.display())
+            })
+            .inspect_err(|e| {
+                // TODO(mkaput): Send a notification to the language client about the error.
+                error!("{e:?}");
+            })
+            .ok();
+
+        if project_config.is_some() {
+            self.project_config = project_config;
+        }
+    }
+
+    fn requires_unmanaged_core(&self) -> bool {
+        true
+    }
+
+    fn apply_db_changes(&self, db: &mut RootDatabase) {
+        if let Some(project_config) = self.project_config.as_ref() {
+            update_crate_roots_from_project_config(db, project_config);
+        }
+    }
+}

crates/cairo-lang-language-server/src/project/scarb/mod.rs
@@ -1,2 +1,113 @@
-// TODO(mkaput): This is `pub` temporarily.
-pub(crate) mod db;
+use std::path::{Path, PathBuf};
+
+use anyhow::Context;
+use cairo_lang_compiler::db::RootDatabase;
+use scarb_metadata::Metadata;
+use tracing::{error, trace, warn};
+
+use super::Project;
+use crate::toolchain::scarb::{ScarbToolchain, SCARB_LOCK, SCARB_TOML};
+
+mod db;
+
+/// A [`Project`] backed by a Scarb workspace (`Scarb.toml` file).
+pub struct ScarbWorkspace {
+    /// Path to the top-most Scarb manifest file (i.e., the **workspace** one).
+    manifest_path: PathBuf,
+
+    /// Scarb toolchain to use in this workspace.
+    scarb_toolchain: ScarbToolchain,
+
+    /// Path to the lock file of this workspace.
+    lock_path: PathBuf,
+
+    /// Last known revision of `scarb metadata` command output.
+    metadata: Option<Metadata>,
+}
+
+impl ScarbWorkspace {
+    /// Constructs a new [`ScarbWorkspace`] and triggers `scarb metadata` loading.
+    ///
+    /// The `manifest_path` argument can point to either a package `Scarb.toml` or a workspace one.
+    /// If input package happens to be part of a broader workspace, this object will adapt to it.
+    pub fn initialize(manifest_path: PathBuf, scarb_toolchain: &ScarbToolchain) -> Self {
+        assert_eq!(manifest_path.file_name().and_then(|s| s.to_str()), Some(SCARB_TOML));
+
+        let lock_path = manifest_path.with_file_name(SCARB_LOCK);
+        let mut this = Self {
+            manifest_path,
+            scarb_toolchain: scarb_toolchain.clone(),
+            lock_path,
+            metadata: None,
+        };
+
+        this.do_reload();
+
+        this
+    }
+
+    /// Calls `scarb metadata`, parses its result and stores it in the `metadata` field.
+    // FIXME(mkaput): This should actually invoke `scarb metadata` in a background task and not
+    //  block the main thread.
+    #[tracing::instrument(level = "debug", skip_all, fields(manifest = ?self.manifest_path))]
+    fn do_reload(&mut self) {
+        let metadata = self
+            .scarb_toolchain
+            .metadata(&self.manifest_path)
+            .with_context(|| {
+                format!("failed to reload scarb workspace: {}", self.manifest_path.display())
+            })
+            .inspect_err(|e| {
+                // TODO(mkaput): Send a notification to the language client about the error.
+                error!("{e:?}");
+            })
+            .ok();
+
+        if let Some(metadata) = metadata {
+            // It is possible that this workspace was initialized with a path to workspace
+            // member (we cannot be sure until we execute `scarb metadata`).
+            // If this is true, change `manifest_path` to the real workspace manifest path.
+            self.manifest_path = metadata.workspace.manifest_path.clone().into();
+
+            self.lock_path = metadata.workspace.manifest_path.with_file_name(SCARB_LOCK).into();
+
+            self.metadata = Some(metadata);
+        }
+    }
+}
+
+impl Project for ScarbWorkspace {
+    fn manifest_files(&self) -> Vec<&Path> {
+        let Some(metadata) = &self.metadata else {
+            return vec![&self.manifest_path];
+        };
+
+        [metadata.workspace.manifest_path.as_std_path(), self.lock_path.as_path()]
+            .into_iter()
+            .chain(
+                metadata
+                    .workspace
+                    .members
+                    .iter()
+                    .flat_map(|pkg_id| metadata.get_package(pkg_id))
+                    .map(|pkg| pkg.manifest_path.as_std_path()),
+            )
+            .collect()
+    }
+
+    fn reload(&mut self) {
+        self.do_reload();
+    }
+
+    fn requires_unmanaged_core(&self) -> bool {
+        self.metadata.is_none()
+    }
+
+    fn apply_db_changes(&self, db: &mut RootDatabase) {
+        if let Some(metadata) = &self.metadata {
+            db::update_crate_roots(metadata, db);
+        } else {
+            trace!("metadata has not been yet loaded, skipping");
+        }
+    }
+}

crates/cairo-lang-language-server/src/project/unmanaged_core_crate.rs
@@ -5,26 +5,48 @@ use tracing::warn;
 
 use crate::config::Config;
 
-/// Try to find a Cairo `core` crate (see [`find_unmanaged_core`]) and initialize it in the
-/// provided database.
-pub fn try_to_init_unmanaged_core(config: &Config, db: &mut (dyn FilesGroup + 'static)) {
-    if let Some(path) = find_unmanaged_core(config) {
-        init_dev_corelib(db, path);
-    } else {
-        warn!("failed to find unmanaged core crate")
-    }
+/// A helper object for managing the unmanaged Cairo `core` crate.
+///
+/// An unmanaged `core` is one that is used in projects whose managers do not handle the `core`
+/// crate themselves.
+/// This is the case for `cairo_project.toml`-based projects and for detached files.
+pub struct UnmanagedCoreCrate {
+    fallback_path: Option<PathBuf>,
 }
 
-/// Try to find a Cairo `core` crate in various well-known places, for use in project backends that
-/// do not manage the `core` crate (i.e., anything non-Scarb).
-pub fn find_unmanaged_core(config: &Config) -> Option<PathBuf> {
-    // TODO(mkaput): First, try to find Scarb-managed `core` package if we have Scarb toolchain.
-    //   The most reliable way to do this is to create an empty Scarb package, and run
-    //   `scarb metadata` on it. The `core` package will be a component of this empty package.
-    //   For minimal packages, `scarb metadata` should be pretty fast.
-    // TODO(mkaput): Shouldn't `fallback` actually be an `override`?
-    cairo_lang_filesystem::detect::detect_corelib()
-        .or_else(|| find_core_at_path(config.unmanaged_core_fallback_path.as_ref()?.as_path()))
+impl UnmanagedCoreCrate {
+    /// Constructs a new [`UnmanagedCoreCrate`].
+    pub fn new(config: &Config) -> Self {
+        Self { fallback_path: config.unmanaged_core_fallback_path.clone() }
+    }
+
+    /// Reacts to changes in the [`Config`].
+    pub fn on_config_changed(&mut self, config: &Config) {
+        self.fallback_path.clone_from(&config.unmanaged_core_fallback_path);
+    }
+
+    /// Tries to find a Cairo `core` crate and initializes it in the provided database.
+    pub fn apply_db_changes(&self, db: &mut (dyn FilesGroup + 'static)) {
+        if let Some(path) = self.discover() {
+            // FIXME(mkaput): This should actually read from cairo_project.toml/Scarb.toml.
+            //   init_dev_corelib has to die.
+            init_dev_corelib(db, path);
+        } else {
+            warn!("failed to find unmanaged core crate")
+        }
+    }
+
+    /// Tries to find a Cairo `core` crate in various well-known places, for use in project backends
+    /// that do not manage the `core` crate (i.e., anything non-Scarb).
+    fn discover(&self) -> Option<PathBuf> {
+        // TODO(mkaput): First, try to find Scarb-managed `core` package if we have Scarb toolchain.
+        //   The most reliable way to do this is to create an empty Scarb package, and run
+        //   `scarb metadata` on it. The `core` package will be a component of this empty package.
+        //   For minimal packages, `scarb metadata` should be pretty fast.
+        // TODO(mkaput): Shouldn't `fallback` actually be an `override`?
+        cairo_lang_filesystem::detect::detect_corelib()
+            .or_else(|| find_core_at_path(self.fallback_path.as_ref()?.as_path()))
+    }
 }
 
 /// Attempts to find the `core` crate source root at a given path.

wl4g-blockchain/starknet-cairo-compiler

crates/cairo-lang-language-server/src/project/cairo_project.rs
@@ -1,0 +1,60 @@
+use std::path::{Path, PathBuf};
+
+use anyhow::Context;
+use cairo_lang_compiler::project::update_crate_roots_from_project_config;
+use cairo_lang_project::{ProjectConfig, PROJECT_FILE_NAME};
+use tracing::error;
+
+use super::Project;
+use crate::lang::db::AnalysisDatabase;
+
+/// A [`Project`] manually specified using a `cairo_project.toml` file.
+pub struct CairoProject {
+    /// Path to the `cairo_project.toml` file.
+    project_path: PathBuf,
+
+    /// Last known revision of `cairo_project.toml` contents, parsed.
+    project_config: Option<ProjectConfig>,
+}
+
+impl CairoProject {
+    /// Constructs a new [`CairoProject`] and loads it.
+    pub fn initialize(project_path: PathBuf) -> Self {
+        assert_eq!(project_path.file_name().and_then(|s| s.to_str()), Some(PROJECT_FILE_NAME));
+        let mut this = Self { project_path, project_config: None };
+        this.reload();
+        this
+    }
+}
+
+impl Project for CairoProject {
+    fn manifest_files(&self) -> Vec<&Path> {
+        vec![&self.project_path]
+    }
+
+    fn reload(&mut self) {
+        let project_config = ProjectConfig::from_file(&self.project_path)
+            .with_context(|| {
+                format!("failed to reload cairo project: {}", self.project_path.display())
+            })
+            .inspect_err(|e| {
+                // TODO(mkaput): Send a notification to the language client about the error.
+                error!("{e:?}");
+            })
+            .ok();
+
+        if project_config.is_some() {
+            self.project_config = project_config;
+        }
+    }
+
+    fn requires_unmanaged_core(&self) -> bool {
+        true
+    }
+
+    fn apply_db_changes(&self, db: &mut AnalysisDatabase) {
+        if let Some(project_config) = self.project_config.as_ref() {
+            update_crate_roots_from_project_config(db, project_config);
+        }
+    }
+}

crates/cairo-lang-language-server/src/project/scarb/mod.rs
@@ -1,2 +1,113 @@
-// TODO(mkaput): This is `pub` temporarily.
-pub(crate) mod db;
+use std::path::{Path, PathBuf};
+
+use anyhow::Context;
+use scarb_metadata::Metadata;
+use tracing::{error, trace, warn};
+
+use super::Project;
+use crate::lang::db::AnalysisDatabase;
+use crate::toolchain::scarb::{ScarbToolchain, SCARB_LOCK, SCARB_TOML};
+
+mod db;
+
+/// A [`Project`] backed by a Scarb workspace (`Scarb.toml` file).
+pub struct ScarbWorkspace {
+    /// Path to the top-most Scarb manifest file (i.e., the **workspace** one).
+    manifest_path: PathBuf,
+
+    /// Scarb toolchain to use in this workspace.
+    scarb_toolchain: ScarbToolchain,
+
+    /// Path to the lock file of this workspace.
+    lock_path: PathBuf,
+
+    /// Last known revision of `scarb metadata` command output.
+    metadata: Option<Metadata>,
+}
+
+impl ScarbWorkspace {
+    /// Constructs a new [`ScarbWorkspace`] and triggers `scarb metadata` loading.
+    ///
+    /// The `manifest_path` argument can point to either a package `Scarb.toml` or a workspace one.
+    /// If input package happens to be part of a broader workspace, this object will adapt to it.
+    pub fn initialize(manifest_path: PathBuf, scarb_toolchain: &ScarbToolchain) -> Self {
+        assert_eq!(manifest_path.file_name().and_then(|s| s.to_str()), Some(SCARB_TOML));
+
+        let lock_path = manifest_path.with_file_name(SCARB_LOCK);
+        let mut this = Self {
+            manifest_path,
+            scarb_toolchain: scarb_toolchain.clone(),
+            lock_path,
+            metadata: None,
+        };
+
+        this.do_reload();
+
+        this
+    }
+
+    /// Calls `scarb metadata`, parses its result and stores it in the `metadata` field.
+    // FIXME(mkaput): This should actually invoke `scarb metadata` in a background task and not
+    //  block the main thread.
+    #[tracing::instrument(level = "debug", skip_all, fields(manifest = ?self.manifest_path))]
+    fn do_reload(&mut self) {
+        let metadata = self
+            .scarb_toolchain
+            .metadata(&self.manifest_path)
+            .with_context(|| {
+                format!("failed to reload scarb workspace: {}", self.manifest_path.display())
+            })
+            .inspect_err(|e| {
+                // TODO(mkaput): Send a notification to the language client about the error.
+                error!("{e:?}");
+            })
+            .ok();
+
+        if let Some(metadata) = metadata {
+            // It is possible that this workspace was initialized with a path to workspace
+            // member (we cannot be sure until we execute `scarb metadata`).
+            // If this is true, change `manifest_path` to the real workspace manifest path.
+            self.manifest_path = metadata.workspace.manifest_path.clone().into();
+
+            self.lock_path = metadata.workspace.manifest_path.with_file_name(SCARB_LOCK).into();
+
+            self.metadata = Some(metadata);
+        }
+    }
+}
+
+impl Project for ScarbWorkspace {
+    fn manifest_files(&self) -> Vec<&Path> {
+        let Some(metadata) = &self.metadata else {
+            return vec![&self.manifest_path];
+        };
+
+        [metadata.workspace.manifest_path.as_std_path(), self.lock_path.as_path()]
+            .into_iter()
+            .chain(
+                metadata
+                    .workspace
+                    .members
+                    .iter()
+                    .flat_map(|pkg_id| metadata.get_package(pkg_id))
+                    .map(|pkg| pkg.manifest_path.as_std_path()),
+            )
+            .collect()
+    }
+
+    fn reload(&mut self) {
+        self.do_reload();
+    }
+
+    fn requires_unmanaged_core(&self) -> bool {
+        self.metadata.is_none()
+    }
+
+    fn apply_db_changes(&self, db: &mut AnalysisDatabase) {
+        if let Some(metadata) = &self.metadata {
+            db::update_crate_roots(metadata, db);
+        } else {
+            trace!("metadata has not been yet loaded, skipping");
+        }
+    }
+}

crates/cairo-lang-language-server/src/project/unmanaged_core_crate.rs
@@ -8,33 +8,56 @@ use tracing::{error, warn};
 use crate::config::Config;
 use crate::lang::db::AnalysisDatabase;
 
-/// Try to find a Cairo `core` crate (see [`find_unmanaged_core`]) and initialize it in the
-/// provided database.
-pub fn try_to_init_unmanaged_core(config: &Config, db: &mut AnalysisDatabase) {
-    if let Some(path) = find_unmanaged_core(config) {
-        init_dev_corelib(db, path);
-    } else {
-        warn!("failed to find unmanaged core crate")
+/// A helper object for managing the unmanaged Cairo `core` crate.
+///
+/// An unmanaged `core` is one that is used in projects whose managers do not handle the `core`
+/// crate themselves.
+/// This is the case for `cairo_project.toml`-based projects and for detached files.
+pub struct UnmanagedCoreCrate {
+    core_path: Option<PathBuf>,
+}
+
+impl UnmanagedCoreCrate {
+    /// Constructs a new [`UnmanagedCoreCrate`].
+    pub fn new(config: &Config) -> Self {
+        Self { core_path: config.unmanaged_core_path.clone() }
+    }
+
+    /// Reacts to changes in the [`Config`].
+    pub fn on_config_changed(&mut self, config: &Config) {
+        self.core_path.clone_from(&config.unmanaged_core_path);
+    }
+
+    /// Tries to find a Cairo `core` crate and initializes it in the provided database.
+    pub fn apply_db_changes(&self, db: &mut AnalysisDatabase) {
+        if let Some(path) = self.discover() {
+            // FIXME(mkaput): This should actually read from cairo_project.toml/Scarb.toml.
+            //   init_dev_corelib has to die.
+            init_dev_corelib(db, path);
+        } else {
+            warn!("failed to find unmanaged core crate")
+        }
+    }
+
+    /// Tries to find a Cairo `core` crate in various well-known places, for use in project backends
+    /// that do not manage the `core` crate (i.e., anything non-Scarb).
+    ///
+    /// The path is guaranteed to be absolute, so it can be safely used as a `FileId` in LS Salsa
+    /// DB.
+    fn discover(&self) -> Option<PathBuf> {
+        // TODO(mkaput): First, try to find Scarb-managed `core` package if we have Scarb toolchain.
+        //   The most reliable way to do this is to create an empty Scarb package, and run
+        //   `scarb metadata` on it. The `core` package will be a component of this empty package.
+        //   For minimal packages, `scarb metadata` should be pretty fast.
+        find_core_at_config_path(&self.core_path)
+            .or_else(cairo_lang_filesystem::detect::detect_corelib)
+            .and_then(ensure_absolute)
     }
 }
 
-/// Try to find a Cairo `core` crate in various well-known places, for use in project backends that
-/// do not manage the `core` crate (i.e., anything non-Scarb).
-///
-/// The path is guaranteed to be absolute, so it can be safely used as a `FileId` in LS Salsa DB.
-pub fn find_unmanaged_core(config: &Config) -> Option<PathBuf> {
-    // TODO(mkaput): First, try to find Scarb-managed `core` package if we have Scarb toolchain.
-    //   The most reliable way to do this is to create an empty Scarb package, and run
-    //   `scarb metadata` on it. The `core` package will be a component of this empty package.
-    //   For minimal packages, `scarb metadata` should be pretty fast.
-    find_core_at_config_path(config)
-        .or_else(cairo_lang_filesystem::detect::detect_corelib)
-        .and_then(ensure_absolute)
-}
-
 /// Attempts to find the `core` crate source root at the path provided in the configuration.
-fn find_core_at_config_path(config: &Config) -> Option<PathBuf> {
-    find_core_at_path(config.unmanaged_core_path.as_ref()?.as_path())
+fn find_core_at_config_path(core_path: &Option<PathBuf>) -> Option<PathBuf> {
+    find_core_at_path(core_path.as_ref()?.as_path())
 }
 
 /// Attempts to find the `core` crate source root at a given path.